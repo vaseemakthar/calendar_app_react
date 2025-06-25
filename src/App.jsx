14f94a31500259ccd93c3cacd8d4ba8b27aaed89
VASEEM AKTHAR A <vaseemakthar31@gmail.com>
2025-06-25 14:01:28 +0530

    functionality

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ import CalenderApp from "./Components/CalenderApp"
+ import './Components/CalenderApp.css'
+
+ const App = () => {
+   return (
+     <div className="container">
+       <CalenderApp />
+     </div>
+   )
+ }
+ 
+ export default App
+ 
